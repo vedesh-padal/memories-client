6549b99629b9703cb13dff40eb0d54cab9d358ce
Vedesh Padal <92447640+vedesh-padal@users.noreply.github.com>
2024-04-11 13:19:37 +0530

    Update PostDetails.jsx
    deletion of unimplemented feature

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -54,8 +54,6 @@ const PostDetails = () => {
 					<Typography variant="h6">Created by: {post.name}</Typography>
 					<Typography variant="body1">{moment(post.createdAt).fromNow()}</Typography>
 					<Divider style={{ margin: '20px 0' }} />
-					<Typography variant="body1"><strong>Realtime Chat - coming soon!</strong></Typography>
-					<Divider style={{ margin: '20px 0' }} />
 					<CommentSection post={post} />
 					<Divider style={{ margin: '20px 0' }} />
 				</div>
